dc040d4c9fc26ed937556289867debc63a8396b1
Sasha Stepanova <chernishevskie@yandex.ru>
2022-06-21 20:47:08 +0300

    feat: added component Catigories

---
+++ src/components/catigories/Catigories.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Catigories() {
+  return (
+    <section className="catigories">
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </section>
+  );
+}
+
+export default Catigories;
