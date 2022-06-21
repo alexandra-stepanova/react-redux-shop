04fe35f8738e4a0cb99bf4c4dd6a5276e82a3641
Sasha Stepanova <chernishevskie@yandex.ru>
2022-06-21 20:48:41 +0300

    feat: addde component Catigories

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../header/Header';
+import Catigories from '../catigories/Catigories';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Catigories />
     </div>
   );
 }
