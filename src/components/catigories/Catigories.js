b4936447820c9d4bd5d3dc73d308ed0470e6fe58
Sasha Stepanova <chernishevskie@yandex.ru>
2022-06-21 21:56:20 +0300

    refactor: changed layout

--- src/components/catigories/Catigories.js
+++ src/components/catigories/Catigories.js
@@ -2,14 +2,14 @@ import React from "react";
 
 function Catigories() {
   return (
-    <section className="catigories">
+    <section className="categories">
       <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li className="active">Все</li>
+        <li>Мясные</li>
+        <li>Вегетарианская</li>
+        <li>Гриль</li>
+        <li>Острые</li>
+        <li>Закрытые</li>
       </ul>
     </section>
   );
