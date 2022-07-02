e589add528f54704e2dc0df48cb4fa3ea0eb67c9
Sasha Stepanova <chernishevskie@yandex.ru>
2022-07-03 00:58:50 +0300

    refactor: added routers to the app

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,12 +1,17 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Header from "../header/Header";
 import Main from "../main/Main";
+import Cart from "../cart/Cart"
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <Main />
+      <Routes >
+        <Route path="/" element={<Main/>}/>
+        <Route path="/cart" element={<Cart/>}/>
+      </Routes>
     </div>
   );
 }
