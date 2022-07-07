ada9fd47d27bb8d2bdfbcc17a482837dc3b2aced
Sasha Stepanova <chernishevskie@yandex.ru>
2022-07-07 20:56:13 +0300

    refactor: changed name + added array categories

---
+++ src/components/categories/Categories.js
@@ -0,0 +1,28 @@
+import React from "react";
+import categories from "../../utils/categories";
+
+function Catigories() {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+
+  const onClickCategory = (index) => {
+    setActiveIndex(index);
+  };
+  return (
+    <section className="categories">
+      <ul className="types">
+        {categories.map((categorie, index) => {
+          return (
+            <li
+              onClick={() => onClickCategory(index)}
+              className={`type ${activeIndex === index? "active" : ""}`}
+            >
+              {categorie}
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+}
+
+export default Catigories;
