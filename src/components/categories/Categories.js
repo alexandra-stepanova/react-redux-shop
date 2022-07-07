64ad727a88e860c4fa43a57d015f2cac436f0e3a
Sasha Stepanova <chernishevskie@yandex.ru>
2022-07-08 00:07:21 +0300

    refactor: simplify onClick function

--- src/components/categories/Categories.js
+++ src/components/categories/Categories.js
@@ -4,17 +4,15 @@ import categories from "../../utils/categories";
 function Catigories() {
   const [activeIndex, setActiveIndex] = React.useState(0);
 
-  const onClickCategory = (index) => {
-    setActiveIndex(index);
-  };
   return (
     <section className="categories">
       <ul className="types">
         {categories.map((categorie, index) => {
           return (
             <li
-              onClick={() => onClickCategory(index)} key={index}
-              className={`type ${activeIndex === index? "active" : ""}`}
+              onClick={() => setActiveIndex(index)}
+              key={index}
+              className={`type ${activeIndex === index ? "active" : ""}`}
             >
               {categorie}
             </li>
