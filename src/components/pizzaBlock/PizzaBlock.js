098f7e6e23dc2c854dd66f79c2cc70e66fc05b62
Sasha Stepanova <chernishevskie@yandex.ru>
2022-07-18 21:38:06 +0300

    refactor: relocated PizzaSkeleton to utils

--- src/components/pizzaBlock/PizzaBlock.js
+++ src/components/pizzaBlock/PizzaBlock.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Pizza from "../pizza/Pizza";
-import PizzaSkeleton from "../pizzaSkeleton/PizzaSkeleton"
+import PizzaSkeleton from "../../utils/pizzaSkeleton/PizzaSkeleton"
 
 function PizzaBlock({ pizzas, isLoading }) {
+  console.log(PizzaSkeleton);
   return (
     <ul className="content__items">
       {pizzas.map((pizza) =>
-        isLoading ? <PizzaSkeleton key={pizza.id} /> : <Pizza key={pizza.id} {...pizza} />
+        isLoading ? (
+          <PizzaSkeleton key={pizza.id} />
+        ) : (
+          <Pizza key={pizza.id} {...pizza} />
+        )
       )}
     </ul>
   );
--- src/utils/pizzaSkeleton/PizzaSkeleton.js
+++ src/utils/pizzaSkeleton/PizzaSkeleton.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ContentLoader from "react-content-loader";
 
-export const PizzaSkeleton = ()  => {
+const PizzaSkeleton = (props) => (
   <ContentLoader
     className="pizza-block"
     speed={2}
@@ -10,12 +10,14 @@ export const PizzaSkeleton = ()  => {
     viewBox="0 0 280 500"
     backgroundColor="#f3f3f3"
     foregroundColor="#ecebeb"
-  >
+  {...props}>
     <circle cx="134" cy="136" r="125" />
     <rect x="0" y="279" rx="10" ry="10" width="280" height="23" />
     <rect x="0" y="326" rx="10" ry="10" width="280" height="88" />
     <rect x="0" y="436" rx="10" ry="10" width="95" height="30" />
     <rect x="125" y="427" rx="24" ry="24" width="152" height="45" />
-  </ContentLoader>;
-}
+  </ContentLoader>
+)
+
+export default PizzaSkeleton;
 
