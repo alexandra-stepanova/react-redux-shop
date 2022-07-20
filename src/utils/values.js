c60b2c12893e7db6bccfc2b6b66615edc3f7ae38
Sasha Stepanova <chernishevskie@yandex.ru>
2022-07-20 11:53:48 +0300

    feat: added more values for better filtering

--- src/utils/values.js
+++ src/utils/values.js
@@ -1,7 +1,10 @@
 const values = [
-  { name: "популярности", sortProperty: "rating" },
-  { name: "цене", sortProperty: "price" },
-  { name: "алфавиту", sortProperty: "name" },
+  { name: "по пулярности (1-5)", sortProperty: "-rating" },
+  { name: "по пулярности (5-1)", sortProperty: "rating" },
+  { name: "по цене (1-1500)", sortProperty: "-price" },
+  { name: "по цене (1500-1)", sortProperty: "price" },
+  { name: "по алфавиту (A-Я)", sortProperty: "-name" },
+  { name: "по алфавиту (Я-А)", sortProperty: "name" },
 ];
 
 export default values;
