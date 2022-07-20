e423cb06a731865813abdf1373ee7aca0fb689e3
Sasha Stepanova <chernishevskie@yandex.ru>
2022-07-20 12:02:25 +0300

    feat: added new component Search

---
+++ src/components/search/Search.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Search() {
+  return (
+    <section className="search">
+      <label className="search-label">
+        <input className="search-input" />
+      </label>
+    </section>
+  );
+}
+
+export default Search;
