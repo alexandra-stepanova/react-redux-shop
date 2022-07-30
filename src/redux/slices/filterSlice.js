587133ee58ad90a83ba2e0ebfe509e7873e2e4e7
Sasha Stepanova <chernishevskie@yandex.ru>
2022-07-30 14:29:14 +0300

    feta: added new method to redux for parsing

--- src/redux/slices/filterSlice.js
+++ src/redux/slices/filterSlice.js
@@ -22,11 +22,16 @@ export const filterSlice = createSlice({
     setCurrentPage: (state, action) => {
       state.currentPage = action.payload;
     },
+    setFilters: (state, action) => {
+      state.categoryId = Number(action.payload.categoryId);
+      state.sortType = action.payload.sortType;
+      state.currentPage = Number(action.payload.currentPage);
+    },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { setCategoryId, setSortType, setCurrentPage } =
+export const { setCategoryId, setSortType, setCurrentPage, setFilters } =
   filterSlice.actions;
 
 export default filterSlice.reducer;
