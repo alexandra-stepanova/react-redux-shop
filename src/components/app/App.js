952f9843b27e4d34e5f06d85391c6a5a58ebdbb4
Sasha Stepanova <chernishevskie@yandex.ru>
2022-07-31 01:42:29 +0300

    refacor: saved the filtering parameters into URL

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,14 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { setFilters } from "../../redux/slices/filterSlice";
+import values from "../../utils/values";
+import qs from "qs";
 import Header from "../header/Header";
 import Main from "../main/Main";
 import Cart from "../cart/Cart";
 import NotFound from "../notFound/NotFound";
 import api from "../../utils/api";
-import { useSelector } from "react-redux";
 
 function App() {
   const { categoryId, sortType, currentPage } = useSelector(
@@ -14,10 +17,13 @@ function App() {
   const [pizzas, setPizzas] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(false);
   const [searchValue, setSearchValue] = React.useState("");
+  let navigate = useNavigate();
+  const dispatch = useDispatch();
+  const sortProperty = sortType.sortProperty;
+  const isSearch = React.useRef(false);
+  const isMounted = React.useRef(false);
 
-  let sortProperty = sortType.sortProperty;
-
-  React.useEffect(() => {
+  const fetchPizza = () => {
     setIsLoading(true);
     api
       .getPizzas(categoryId, sortProperty, searchValue, currentPage)
@@ -26,6 +32,42 @@ function App() {
       })
       .catch((error) => console.log("error", error))
       .finally(() => setTimeout(() => setIsLoading(false), 500));
+  };
+
+  React.useEffect(() => {
+    //если выполнен первый рендер, то параметры не включаются в URL
+    if (isMounted.current) {
+      const queryString = qs.stringify({
+        categoryId,
+        sortProperty,
+        currentPage,
+      });
+      navigate(`?${queryString}`);
+    }
+    setTimeout(() => (isMounted.current = true), 500);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [categoryId, sortProperty, currentPage]);
+
+  React.useEffect(() => {
+    // если параметры получены из адресной строчки, то они вшиваются в фльтры редакса
+    if (window.location.search) {
+      const params = qs.parse(window.location.search.substring(1));
+      const sortType = values.find(
+        (obj) => obj.sortProperty === params.sortProperty
+      );
+      dispatch(setFilters({ ...params, sortType }));
+      isSearch.current = true;
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  React.useEffect(() => {
+    window.scrollTo(0, 0);
+    if (!isSearch.current) {
+      fetchPizza();
+    }
+    isSearch.current = false;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [categoryId, sortProperty, searchValue, currentPage]);
 
   return (
