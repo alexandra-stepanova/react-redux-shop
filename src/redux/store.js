a788a347525931ca5cfeb522f194e3b7e8b29ade
Sasha Stepanova <chernishevskie@yandex.ru>
2022-08-01 00:11:47 +0300

    feat: added cartSlice to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import filterSlice from "./slices/filterSlice";
+import cartSlice from "./slices/cartSlice";
 
 export const store = configureStore({
-  reducer: { filters: filterSlice },
+  reducer: { filters: filterSlice, cart: cartSlice },
 });
