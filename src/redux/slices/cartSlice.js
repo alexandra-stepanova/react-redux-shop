f656cb680c9502a8b43d1bb17a3ccd3be4e621cf
Sasha Stepanova <chernishevskie@yandex.ru>
2022-08-01 19:08:31 +0300

    feat: added counting functionality + state

--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -10,10 +10,15 @@ export const cartSlice = createSlice({
   initialState,
   reducers: {
     addItem(state, action) {
-      state.items.push(action.payload);
-      state.totalPrice = state.items.reduce((sum, obj) => {
-        return obj.price + sum;
-      }, 0);
+      const findItem = state.items.find((obj) => obj.id === action.payload.id);
+      if (findItem) {
+        findItem.count++;
+      } else {
+        state.items.push({ ...action.payload, count: 1 });
+        state.totalPrice = state.items.reduce((sum, obj) => {
+          return obj.price + sum;
+        }, 0);
+      }
     },
     removeItem(state, action) {
       state.items.filter((obj) => obj.id !== action.payload);
