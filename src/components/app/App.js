956f0dfb3b07f5a436e63b28f0c3eba299e31409
Sasha Stepanova <chernishevskie@yandex.ru>
2022-08-06 17:50:27 +0300

    refactor: relocate isLoading and api-request to redux

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,20 +2,19 @@ import React from "react";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { setFilters } from "../../redux/slices/filterSlice";
-import { setPizzas } from "../../redux/slices/pizzasSlice";
+import { fetchPizzas } from "../../redux/slices/pizzasSlice";
 import values from "../../utils/values";
 import qs from "qs";
 import Header from "../header/Header";
 import Main from "../main/Main";
 import Cart from "../cart/Cart";
 import NotFound from "../notFound/NotFound";
-import api from "../../utils/api";
 
 function App() {
   const { categoryId, sortType, currentPage } = useSelector(
     (state) => state.filters
   );
-  const [isLoading, setIsLoading] = React.useState(false);
+  const status = useSelector((state) => state.pizzas.status);
   const [searchValue, setSearchValue] = React.useState("");
   let navigate = useNavigate();
   const dispatch = useDispatch();
@@ -23,22 +22,16 @@ function App() {
   const isSearch = React.useRef(false);
   const isMounted = React.useRef(false);
 
-  const fetchPizza = () => {
-    api
-      .getPizzas(categoryId, sortProperty, searchValue, currentPage)
-      .then((res) => {
-        dispatch(setPizzas(res.data));
-      })
-      .catch((error) => {
-        console.log("error", error);
-      })
-      .finally(() => setTimeout(() => setIsLoading(false), 600));
+  const getPizzas = () => {
+    dispatch(
+      fetchPizzas({ categoryId, sortProperty, searchValue, currentPage })
+    );
   };
 
   React.useEffect(() => {
     window.scrollTo(0, 0);
     if (!isSearch.current) {
-      fetchPizza();
+      getPizzas();
     }
     isSearch.current = false;
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -80,7 +73,9 @@ function App() {
       <Routes>
         <Route
           path="/"
-          element={<Main isLoading={isLoading} currentPage={currentPage} />}
+          element={
+            <Main isLoading={status === "loading"} currentPage={currentPage} />
+          }
         />
         <Route path="/cart" element={<Cart totalCount={totalCount} />} />
         <Route path="/*" element={<NotFound />} />
--- src/redux/slices/pizzasSlice.js
+++ src/redux/slices/pizzasSlice.js
@@ -1,7 +1,28 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchPizzas = createAsyncThunk(
+  "pizzas/fetchPizzasStatus",
+  async (params) => {
+    const { categoryId, sortProperty, searchValue, currentPage } = params;
+    const { data } = await axios.get(
+      `https://62c80ec00f32635590d05660.mockapi.io/items?page=${currentPage}&limit=4&${
+        //пагинация с бэка
+        categoryId > 0 ? `category=${categoryId}` : "" //сортировку по типу
+      }&sortBy=${sortProperty.replace("-", "")}&order=${
+        //сортировка по убыванию/повышению цены, популярности, по алфавиту
+        sortProperty.includes("-") ? "asc" : "desc"
+      }&&${
+        searchValue ? `search=${searchValue}` : "" //фильтрация по поиску слова в header
+      }`
+    );
+    return data;
+  }
+);
 
 const initialState = {
   pizzas: [],
+  status: "loading",
 };
 
 export const pizzasSlice = createSlice({
@@ -12,10 +33,23 @@ export const pizzasSlice = createSlice({
       state.pizzas = action.payload;
     },
   },
+  extraReducers: {
+    [fetchPizzas.pending]: (state) => {
+      state.status = "loading";
+      state.items = [];
+    },
+    [fetchPizzas.fulfilled]: (state, action) => {
+      state.pizzas = action.payload;
+      state.status = "success";
+    },
+    [fetchPizzas.rejected]: (state) => {
+      state.status = "error";
+      state.pizzas = [];
+    },
+  },
 });
 
 // Action creators are generated for each case reducer function
-export const { setPizzas } =
-pizzasSlice.actions;
+export const { setPizzas } = pizzasSlice.actions;
 
 export default pizzasSlice.reducer;
