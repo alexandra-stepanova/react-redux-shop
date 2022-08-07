d7ed536bb466cacfcf9a59bcfbf998fca0c5a30f
Sasha Stepanova <chernishevskie@yandex.ru>
2022-08-07 11:55:14 +0300

    feat: added basic functionality

---
+++ src/components/singlePage/SinglePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useParams } from "react-router";
+
+const SinglePage = () => {
+  return (
+    <div>
+      <img src="#" alt="pizza-picture" />
+      <p></p>
+    </div>
+  );
+};
+
+export default SinglePage;
