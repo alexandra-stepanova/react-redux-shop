cf5bbaf1217b5f0b6b9ac3e83acf2e586999fdf6
Sasha Stepanova <chernishevskie@yandex.ru>
2022-08-07 21:48:31 +0300

    adapted  component EmptyCart into TS

--- src/components/emptyCart/EmptyCart.tsx
+++ src/components/emptyCart/EmptyCart.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import {Link} from 'react-router-dom'
 import emptyCardIcon from '../../assets/images/empty-cart.png'
 
-function EmptyCard() {
+const EmptyCard: React.FC = () => {
   return (
     <section className="content">
       <div className="container container--cart">
         <div className="cart cart--empty">
           <h2>
-            Корзина пустая <icon>😕</icon>
+            Корзина пустая <span>😕</span>
           </h2>
           <p>
             Вероятней всего, вы не заказывали ещё пиццу.
