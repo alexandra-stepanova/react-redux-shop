f2884cdf7c6fffa918928c082b2e484687186cea
Sasha Stepanova <chernishevskie@yandex.ru>
2022-08-07 12:19:57 +0300

    feat: added pizza functionality (axios)

--- src/components/singlePage/SinglePage.js
+++ src/components/singlePage/SinglePage.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import { useParams } from "react-router";
 
 const SinglePage = () => {
+  const [pizza, setPizza] = useState({});
+  const { id } = useParams();
+
+  React.useEffect(() => {
+    async function getPizza() {
+      try {
+        const { data } = await axios.get(
+          `https://62c80ec00f32635590d05660.mockapi.io/items/${id}`
+        );
+        setPizza(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getPizza();
+  }, [id]);
+  console.log(pizza);
   return (
     <div>
-      <img src="#" alt="pizza-picture" />
-      <p></p>
+      <img src={pizza.imageUrl} alt="img" />
+      <p>{pizza.name}</p>
     </div>
   );
 };
