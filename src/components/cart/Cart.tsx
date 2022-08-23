f9b4d6704470f79c92f3c3ec849b43cfc9ed3840
Sasha Stepanova <chernishevskie@yandex.ru>
2022-08-23 11:19:59 +0300

    feat: added typification for totalCount function

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { cartSelector } from "../../redux/slices/cartSlice";
 import EmptyCard from "../emptyCart/EmptyCart";
 import FullCart from "../fullCart/FullCart";
 
 type Carttypes = {
-  totalCount: any;
+  totalCount: (items: []) => number;
 };
 const Cart: React.FC<Carttypes> = ({ totalCount }) => {
-  const { items, totalPrice } = useSelector(cartSelector);
+  const { items, totalPrice } = useSelector((state: any) => state.cart);
   return !items.length ? (
     <EmptyCard />
   ) : (
