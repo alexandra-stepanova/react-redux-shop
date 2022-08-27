ab59185be495b651aa165df5c20f110e9b1c8bf3
Sasha Stepanova <chernishevskie@yandex.ru>
2022-08-27 16:45:37 +0300

    refactor: replaced fetchPizzas request to separate file

--- src/redux/slices/pizza/slice.ts
+++ src/redux/slices/pizza/slice.ts
@@ -1,25 +1,6 @@
-import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
-import axios from "axios";
-import { PizzaItem, FilterSliceFetch, PizzaSliceState, Status } from "./types";
-
-export const fetchPizzas = createAsyncThunk<PizzaItem[], FilterSliceFetch>(
-  "pizzas/fetchPizzasStatus",
-  async (params) => {
-    const { categoryId, sortProperty, searchValue, currentPage } = params;
-    const { data } = await axios.get<PizzaItem[]>(
-      `https://62c80ec00f32635590d05660.mockapi.io/items?page=${currentPage}&limit=4&${
-        //пагинация с бэка
-        categoryId > 0 ? `category=${categoryId}` : "" //сортировку по типу
-      }&sortBy=${sortProperty.replace("-", "")}&order=${
-        //сортировка по убыванию/повышению цены, популярности, по алфавиту
-        sortProperty.includes("-") ? "asc" : "desc"
-      }&&${
-        searchValue ? `search=${searchValue}` : "" //фильтрация по поиску слова в header
-      }`
-    );
-    return data;
-  }
-);
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { fetchPizzas } from "./asyncActions";
+import { PizzaItem, PizzaSliceState, Status } from "./types";
 
 const initialState: PizzaSliceState = {
   pizzas: [],
